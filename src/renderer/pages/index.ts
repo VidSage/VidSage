3738ba6aa29745f2f61a9025d9280d2e4d46f2de
plasmas <plasticplasmas@outlook.com>
2024-12-02 16:48:50 -0500

    fix videoSummary page

--- src/renderer/pages/index.ts
+++ src/renderer/pages/index.ts
@@ -2,12 +2,5 @@ import VideoUpload from './VideoUpload';
 import VideoStoryline from './VideoStoryline';
 import GenerateStoryline from './GenerateStoryline';
 import VideoSummary from './VideoSummary';
-import VideoPreview from './VideoPreview';
 
-export {
-  VideoUpload,
-  VideoStoryline,
-  GenerateStoryline,
-  VideoSummary,
-  VideoPreview,
-};
+export { VideoUpload, VideoStoryline, GenerateStoryline, VideoSummary };
